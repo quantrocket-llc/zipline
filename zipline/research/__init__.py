843f354ee0ea19efb9fb8d6cd4f9cfea0aac233d
Brian <brian@quantrocket.com>
2020-10-14 09:17:25 -0400

    add continuous_future function to zipline.research

--- zipline/research/__init__.py
+++ zipline/research/__init__.py
@@ -1,3 +1,4 @@
 from zipline.research.pipeline import run_pipeline, get_forward_returns
 from zipline.research.bardata import get_data
 from zipline.research.sid import sid
+from zipline.research.continuous_future import continuous_future
---
+++ zipline/research/continuous_future.py
@@ -0,0 +1,83 @@
+#
+# Copyright 2020 QuantRocket LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pandas as pd
+from zipline.data import bundles
+from zipline.utils.extensions import load_extensions
+from zipline.research.exceptions import ValidationError
+from quantrocket.zipline import get_default_bundle
+
+def continuous_future(root_symbol_str, offset=0, roll="volume", adjustment="mul", bundle=None):
+    """
+    Return a ContinuousFuture object for the specified root symbol in the specified bundle
+    (or default bundle).
+
+    Parameters
+    ----------
+    root_symbol_str : str
+        The root symbol for the future chain.
+
+    offset : int, optional
+        The distance from the primary contract. Default is 0.
+
+    roll : str, optional
+        How rolls are determined. Possible choices: 'volume',
+        (roll when back contract volume exceeds front contract
+        volume), or 'calendar' (roll on rollover date). Default
+        is 'volume'.
+
+    adjustment : str, optional
+        Method for adjusting lookback prices between rolls. Possible choices:
+        'mul', 'add', None. Default is 'mul'.
+
+    bundle : str, optional
+        the bundle code. If omitted, the default bundle will be used (and must be set).
+
+    Returns
+    -------
+    asset : zipline.assets.ContinuousFuture
+
+    Examples
+    --------
+    Get the continuous future object for ES and get the current chain as of
+    2020-09-18:
+
+    >>> es = continuous_future("ES", roll="volume", bundle="es-1min")
+    >>> data = get_data("2020-09-18", bundle="es-1min")
+    >>> print(data.current_chain(es))
+    """
+    if not bundle:
+        bundle = get_default_bundle()
+        if not bundle:
+            raise ValidationError("you must specify a bundle or set a default bundle")
+        bundle = bundle["default_bundle"]
+
+    load_extensions(code=bundle)
+
+    bundle_data = bundles.load(
+        bundle,
+        os.environ,
+        pd.Timestamp.utcnow(),
+    )
+
+    continuous_future = bundle_data.asset_finder.create_continuous_future(
+        root_symbol_str,
+        offset,
+        roll,
+        adjustment,
+    )
+
+    return continuous_future
